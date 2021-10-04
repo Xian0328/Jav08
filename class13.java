ac78ef7f7dda9f800e1c6b0171719d244b8c2ffb
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-10-04 11:07:29 +0800

    Add files via upload

--- class13.java
+++ class13.java
@@ -2,17 +2,17 @@ class Circle
 {
 	double pi1=3.14;
 	float pi2=3.14f;
-	void area(double r)
+	void area(double r)      //(a)
 	{
 		double area1=r*r*pi1;
 		System.out.println(area1);
 	}
-	void area(float r)
+	void area(float r)       //(b)
 	{
 		float area2=r*r*pi2;
 		System.out.println(area2);
 	}
-	void area(int r)
+	void area(int r)         //(c)
 	{
 		double area3=r*r*pi1;
 		System.out.println(area3);
@@ -22,7 +22,7 @@ public class class13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Circle radius=new Circle();
+		Circle radius=new Circle();       //(d)
 		radius.area(2.2);
 		radius.area(2.2f);
 		radius.area(2);
